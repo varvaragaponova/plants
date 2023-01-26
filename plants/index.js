3fc41017750bf56a7fd8bc62972689ef722ce283
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-01-26 18:38:54 +0400

    feat: add filter blur on buttons and cards in section service

--- plants/index.js
+++ plants/index.js
@@ -1,4 +1,4 @@
-console.log("75/75\nВёрстка соответствует макету. Ширина экрана 768px +24\nблок <header> +2\nсекция welcome +3\nсекция about +4\nсекция service +4\nсекция prices +4\nсекция contacts +4\nблок <footer> + 3\nВёрстка соответствует макету. Ширина экрана 380px +24\nблок <header> +2\nсекция welcome +3\nсекция about +4\nсекция service +4\nсекция prices +4\nсекция contacts +4\nблок <footer> + 3\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15\nнет полосы прокрутки при ширине страницы от 1440рх до 380px +7\nнет полосы прокрутки при ширине страницы от 380px до 320рх +8\nНа ширине экрана 380рх и меньше реализовано адаптивное меню +22 (Допускается появление адаптивного меня на ширине более 380, но не допускается на ширине более 770px)\nпри ширине страницы 380рх панель навигации скрывается, появляется бургер-иконка +2\nпри нажатии на бургер-иконку плавно появляется адаптивное меню +4\nадаптивное меню соответствует цветовой схеме макета\nпри нажатии на крестик адаптивное меню плавно скрывается уезжая за экран +4\nссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +4\nпри клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна +4");
+//console.log("75/75\nВёрстка соответствует макету. Ширина экрана 768px +24\nблок <header> +2\nсекция welcome +3\nсекция about +4\nсекция service +4\nсекция prices +4\nсекция contacts +4\nблок <footer> + 3\nВёрстка соответствует макету. Ширина экрана 380px +24\nблок <header> +2\nсекция welcome +3\nсекция about +4\nсекция service +4\nсекция prices +4\nсекция contacts +4\nблок <footer> + 3\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15\nнет полосы прокрутки при ширине страницы от 1440рх до 380px +7\nнет полосы прокрутки при ширине страницы от 380px до 320рх +8\nНа ширине экрана 380рх и меньше реализовано адаптивное меню +22 (Допускается появление адаптивного меня на ширине более 380, но не допускается на ширине более 770px)\nпри ширине страницы 380рх панель навигации скрывается, появляется бургер-иконка +2\nпри нажатии на бургер-иконку плавно появляется адаптивное меню +4\nадаптивное меню соответствует цветовой схеме макета\nпри нажатии на крестик адаптивное меню плавно скрывается уезжая за экран +4\nссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям +4\nпри клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, также скрытие меню происходит если сделать клик вне данного окна +4");
 
 const menuButton = document.getElementById('menu');
 const header = document.querySelector('header');
@@ -13,3 +13,46 @@ document.body.addEventListener('click', function() {
         header.classList.remove('open');
     }
 });
+
+const cardChoiceGarden = document.querySelector('.garden');
+const cardChoiceLawn = document.querySelector('.lawn');
+const cardChoicePlanting = document.querySelector('.planting');
+
+const buttonGarden = document.querySelector('.garden-button');
+const buttonLawn = document.querySelector('.lawn-button');
+const buttonPlanting = document.querySelector('.planting-button');
+
+const onServiceButtonClick = (event) => {
+    const currentClass = event && event.target && event.target.className;
+
+    if (!currentClass) return;
+    const buttons = Array.from(document.querySelectorAll('.buttons-wrapper > button'));
+
+    const buttonArr = buttons.filter(button => {
+        return button.className !== currentClass && button.classList.contains('active');
+    });
+
+    if(buttonArr.length === 2) {
+        event.target.classList.remove('active');
+        return;
+    }
+
+    event.target.classList.toggle('active');
+
+    const cards = document.querySelectorAll('.card');
+
+    const activeButtons = buttons.filter(button => {
+        return button.classList.contains('active');
+    }).map(button => button.dataset.class);
+
+    console.log(activeButtons);
+
+    cards.forEach(card => {
+        if(!activeButtons.includes(card.classList[1]) && activeButtons.length) card.classList.add('blur');
+        else if(card.classList.contains('blur')) card.classList.remove('blur');
+    })
+}
+
+buttonGarden.addEventListener('click', onServiceButtonClick);
+buttonLawn.addEventListener('click', onServiceButtonClick);
+buttonPlanting.addEventListener('click', onServiceButtonClick);
